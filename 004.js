865b045b0193c56d7a49d5a67df244c3ca9b58db
edsay12 <edvandearaujo2@hotmail.com>
2023-03-06 23:27:28 -0300

    feat : exercicio: Dois veículos

---
+++ 004.js
@@ -0,0 +1,16 @@
+// 4 - Dois veículos (um carro e um caminhão) saem respectivamente de cidades opostas pela mesma rodovia. O carro de Ribeirão Preto em direção a Franca, a uma velocidade constante de 110 km/h e o caminhão de Franca em direção a Ribeirão Preto a uma velocidade constante de 80 km/h. Quando eles se cruzarem na rodovia, qual estará mais próximo a cidade de Ribeirão Preto?
+
+// a) Considerar a distância de 100km entre a cidade de Ribeirão Preto <-> Franca.
+
+// b) Considerar 2 pedágios como obstáculo e que o caminhão leva 5 minutos a mais para passar em cada um deles e o carro possui tag de pedágio (Sem Parar)
+
+// c) Explique como chegou no resultado.
+
+// Resposta : ambos estão na mesma distancia.
+
+
+// nesse exercicio não usei nenhum codigo nem calculo so usei a logica
+
+// explicação
+// - Se eles vão se cruzar na rodovia, então no exato momento que eles se cruzarem, estarão no mesmo local,
+// ou seja, estarão na mesmo distancia da cidade de Ribeirão preto
